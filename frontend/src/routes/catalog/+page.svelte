2e527d8b647157b213f3a6f729015311d058b8b3
Julian <yizman@list.ru>
2024-10-05 20:58:07 +0500

    add: component for catalog page

--- frontend/src/routes/catalog/+page.svelte
+++ frontend/src/routes/catalog/+page.svelte
@@ -2,16 +2,6 @@
 	import CatalogBody from "../../components/catalog/CatalogBody.svelte";
 	import Footer from "../../components/homepage/Footer.svelte";
     import Header from "../../components/homepage/header.svelte";
-    import { onMount } from 'svelte';
-    
-    let brand_id;
-
-    onMount(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        brand_id = urlParams.get('brand_id');
-        console.log(brand_id);
-    });
-
 </script>
 
 
