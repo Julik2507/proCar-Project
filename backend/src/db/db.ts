2e2b13eccc3118dc7005c6093187fe841284416e
Julian <yizman@list.ru>
2025-01-08 14:06:51 +0500

    bugfix

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,9 +1,11 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema.js';
 import pkg from 'pg';
+import 'dotenv/config';
+
 
 const pool = new pkg.Pool({
-    connectionString: 'postgres://julian:yulian20@176.109.107.106:5432/proCars'
+    connectionString: process.env.DATABASE
 });
 
 export const db = drizzle(pool, { schema });
